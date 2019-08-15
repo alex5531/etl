113fc02dd2ffb2cd0637ca6d9aca7c9631e6b3a2
alex5531 <54130858+alex5531@users.noreply.github.com>
2019-08-15 01:18:16 -0400

    Add files via upload

---
+++ etl/Interface/IDest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace etl.Interface
+{
+    public interface IDest
+    {
+        void TransfDestContext(Action action);
+        void Process(Dictionary<string, object> destRow);
+    }
+}
